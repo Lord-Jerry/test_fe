3eb992d0717543a245d629f4bb87d17c7546bee8
Lord Jerry <lovinjerry004@gmail.com>
2019-12-13 11:14:46 +0100

    add build foler to fix websocket issue

---
+++ build/precache-manifest.4d452a77adfbb5feb6ec0973fc36668c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796fc5d276feecd1f56a37429855aec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5390e330dbbaf76cc5f",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "556ffa8fe67788cca892",
+    "url": "/static/css/main.ae590a29.chunk.css"
+  },
+  {
+    "revision": "c5390e330dbbaf76cc5f",
+    "url": "/static/js/2.8484631b.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.8484631b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "556ffa8fe67788cca892",
+    "url": "/static/js/main.e3be0c9f.chunk.js"
+  },
+  {
+    "revision": "21a8b84d5c5b03d28e2e",
+    "url": "/static/js/runtime-main.89498550.js"
+  }
+]);
